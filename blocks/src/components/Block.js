4b9fba4d39f7c044c78fa54b00e7a4e7e530cf82
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-10-13 00:06:48 -0700

    initial commit

--- blocks/src/components/Block.js
+++ blocks/src/components/Block.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Slot from './Slot.js'
 import blockTypes from '../templates/block_types.js'
+import Draggable from 'react-draggable';
 
 class Block extends Component {
+  constructor(){
+    super()
+    this.checkPosition = this.checkPosition.bind(this)
+  }
+  checkPosition(){
+    var node = ReactDOM.findDOMNode(this);
+    console.log(window.getComputedStyle(node).transform);
+  }
+
   render() {
     let testBlock = Object.values(blockTypes)[Math.floor(Math.random()*15)];    
     let renderBlock = [];
@@ -19,9 +30,14 @@ class Block extends Component {
       }
     })
     return (
+      <Draggable
+        onDrag={this.checkPosition}
+      >
       <div className="block">
         {renderBlock}
+        HII
       </div>
+      </Draggable>
     );
   }
 }
